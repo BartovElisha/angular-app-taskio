8a7c38c2c627204564f4471f4024e9eb0c16febf
ElishaBartov <elisha.bartov@emerson.com>
2023-02-23 17:41:39 +0200

    Added to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app-taskio';
+  today = new Date();
+
+  getImagePath() {
+    return "https://cdn.pixabay.com/photo/2023/01/31/05/59/zebra-7757193_640.jpg";
+  }
+
+  getTaskById(id: number) {
+    const tasks = [
+      'Your first task',
+      'Your second task',
+      'Another task'
+    ];
+
+    return tasks[id];
+  }
 }
---
+++ src/app/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+
+  doSomething(message: string) {
+    console.log(message);
+  }
+
+}
